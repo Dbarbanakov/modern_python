0c48bb65fd39c3c1e467b0e2e30860a42a5f2c1a
dbarbanakov <dbarbanakov@gmail.com>
2026-04-06 21:28:57 +0300

    Enhance error handling in API data retrieval and add test for request errors when there is no network.

--- src/modern_python/get_data.py
+++ src/modern_python/get_data.py
@@ -1,12 +1,16 @@
 # src/hypermodern-python/get_data.py
+import click
 import requests
 
 
 API_URL = 'https://jsonplaceholder.typicode.com/todos'
 
 
-
 def api_data():
-    with requests.get(API_URL) as response:
-        response.raise_for_status()
-        return response.json()
+    try:
+        with requests.get(API_URL) as response:
+            response.raise_for_status()
+            return response.json()
+    except requests.RequestException as error:
+        message = str(error)
+        raise click.ClickException(message)
